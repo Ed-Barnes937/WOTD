ac68cb7a8767b767379e7417cab09cd045553a29
ed-barnes937 <ed.barnes937@gmail.com>
2024-11-28 23:18:07 +0000

    welcome message update

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -11,14 +11,14 @@ function Home() {
   const user = useAuthStore((store) => store.user);
 
   return (
-    <div className="p-6 flex flex-col justify-center items-center">
+    <div className="px-6 py-2 flex flex-col justify-center items-center">
       {user && (
-        <Typography variant={"h2"}>
+        <Typography variant={"h3"} className="mt-1">
           Welcome back {user.user_metadata.name}!
         </Typography>
       )}
-      <Typography variant="h2" className="mb-4">
-        Choose your learning level!
+      <Typography variant="h2" className="mb-4 mt-4">
+        Pick a level, any level!
       </Typography>
       <div className="grid grid-cols-2 gap-4">
         {WOTDLevelMap.map((level) => (
